a361dd378e09ebb71081ab242b961e354bdfd09a
NewPan <newyjp@users.noreply.github.com>
2017-10-07 10:06:17 -0500

    Update main.cpp

--- 05-Binary-Search-Tree/Course Code (C++)/Optional-01-More-About-Binary-Search/main.cpp
+++ 05-Binary-Search-Tree/Course Code (C++)/Optional-01-More-About-Binary-Search/main.cpp
@@ -8,6 +8,20 @@ using namespace std;
 // 如果找到target, 返回第一个target相应的索引index
 // 如果没有找到target, 返回比target小的最大值相应的索引, 如果这个最大值有多个, 返回最大索引
 // 如果这个target比整个数组的最小元素值还要小, 则不存在这个target的floor值, 返回-1
+/*
+最大的问题在于，当arr[mid] < target的时候，此时arr[mid]有可能就是我们要求的floor值。
+比如数组[..., 1, 3, ...]，target=2；当arr[mid]指向1的时候，此时1就是这个数组对于2的floor值。
+但是你的逻辑依然让l=mid+1，相当于错过了这个值。应该让l=mid，也就是让l留在这里，然后继续观察看会不会有更合适的值。
+
+当我们让l=mid的时候，会有一个很微妙的问题：当l=a，r=a+1这种情况的时候，由于下取整，mid计算出来仍然是a==l，
+此时我们的程序会进入一个死循环（随便找一个例子打印出循环每次的l，r和mid调试试试看：））。
+要打破这个死循环，我们需要在l=a, r=a+1这种情况下，计算出的mid是r的值，这样可以执行r=mid-1，进而在下次while循环里退出。
+所以在floor中，我们计算mid的方式是使用上取整，即mid=(l+r+1)/2。为了防止溢出，我的代码写成mid = l + (r-l+1)/2。
+
+这样计算出的arr[l]，将是比target小的最大的元素。之后我们来看arr[l+1]是不是target，如果是，我们的floor值取l+1的位置，
+否则就是l的位置。这是根据floor的定义来的。当然，在有些情况，我们可能就是想求出比target小的最大元素（即使arr中含有target），
+那么这一步就可以免掉了。
+*/
 template<typename T>
 int floor(T arr[], int n, T target){
 
